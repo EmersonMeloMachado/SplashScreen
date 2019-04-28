03d388615aed6738b280c6f4fb210e038de037d9
Emerson Melo Machado <emerson86melomachado@hotmail.com>
2019-04-28 12:35:11 -0300

    Criação e configuração projeto android com Splah Screen Lottie

---
+++ SplashScreen/SplashScreen/SplashScreen.Android/SplashScreenActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.Animation;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Com.Airbnb.Lottie;
+
+namespace SplashScreen.Droid
+{
+    [Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashScreenActivity : Activity, Animator.IAnimatorListener
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Activity_SplashScreen);
+
+            LottieAnimationView animationView = FindViewById<LottieAnimationView>(Resource.Id.animation_view);
+            animationView.AddAnimatorListener(this);
+        }
+
+        public void OnAnimationCancel(Animator animation)
+        {
+        }
+
+        public void OnAnimationEnd(Animator animation)
+        {
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        public void OnAnimationRepeat(Animator animation)
+        {
+        }
+
+        public void OnAnimationStart(Animator animation)
+        {
+        }
+    }
+}
